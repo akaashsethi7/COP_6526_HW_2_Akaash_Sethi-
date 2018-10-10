5d3b7f9edcc928cde7a8969838b535ba736ade31
Akaash Sethi <Akaash.A.Sethi@disney.com>
2018-10-10 01:07:34 -0400

    Implemented lower casing and removal of punctuation

--- word_counter_akaash.py
+++ word_counter_akaash.py
@@ -1,24 +1,55 @@
-from pyspark import SparkContext
-from pyspark import SparkConf
 
-def count_lines_lambdaExpression():
-  '''
-  The function is to compute the number of line in a text file.
-  The function is implemented with python lambda expression.
-  '''
-  # configuration
-  APP_NAME = 'count lines'
-  conf = SparkConf().setAppName(APP_NAME)
-  conf = conf.setMaster('spark://ec2-18-206-64-64.compute-1.amazonaws.com:7077')
-  sc = SparkContext(conf=conf)
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-  # actuall lambda
-  lines = sc.textFile('/input/README.txt')
-  lineLength = lines.map(lambda s: len(s))
-  totalLength = lineLength.reduce(lambda a,b: a+b)
-  print ("The total length of the document is {}".format(totalLength))
-  return totalLength
-  pass
 
-if __name__ == '__main__':
-    print count_lines_lambdaExpression()
+
+# Updated original wordcount.py script for COP 6526 Homework 2 Assignment 
+
+from __future__ import print_function
+
+import sys
+import re
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: wordcount <file>", file=sys.stderr)
+        exit(-1)
+
+    spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+#    counts = lines.flatMap(lambda x: x.split(' ')) \
+#                  .map(lambda x: (x, 1)) \
+#                  .reduceByKey(add)
+#    output = counts.collect()
+#    for (word, count) in output:
+#        print("%s: %i" % (word, count))
+    counts = lines.map(lambda x:x.lower()) \
+                  .map(lambda x:re.sub(r"[,.;@#?!&$]+\ *","",x)) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+
+    spark.stop()
